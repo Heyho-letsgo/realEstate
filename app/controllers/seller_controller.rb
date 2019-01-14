14205c8d0c88632044c3c068451b50e206b9ed12
Baudin-Laurencin <abaudin@gmail.com>
2019-01-14 08:48:01 +0000

    Ajout controller Seller

---
+++ app/controllers/seller_controller.rb
@@ -0,0 +1,26 @@
+class SellerController < ApplicationController
+
+   def list
+   end
+   
+   def show
+   end
+   
+   def new
+   end
+   
+   def create
+   end
+   
+   def edit
+   end
+   
+   def update
+   end
+   
+   def delete
+   end
+
+
+
+end
