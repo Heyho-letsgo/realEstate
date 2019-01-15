66dca08c695fba683a21a8003c56fa41a42ebc8a
Baudin-Laurencin <abaudin@gmail.com>
2019-01-15 15:16:05 +0000

    En attente de création d'un buyer

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -8,34 +8,36 @@ class BuyersController < ApplicationController
         @buyers = Buyer.search(search_room, search_bedroom)
         
         
-        #https://stackoverflow.com/questions/44504983/search-multiple-fields-with-multiple-values
-        #Buyer.search(params[:room])
+    #https://stackoverflow.com/questions/44504983/search-multiple-fields-with-multiple-values
+    # https://rubyplus.com/articles/3381-Simple-Search-Form-in-Rails-5
     end      
        
 
-   def show
+    def show
        @buyer = Buyer.find(params[:id])
+    end
+    
+    def new
+    end
+ 
+    def create
+       @buyer = Buyer.new(params[:room, :bedroom])
        
-       
-   end
+       @buyer.save
+       redirect _to "/buyers"
+    end
    
-   def new
-   end
+    def edit
+    end
    
-   def create
-   end
+    def update
+    end
    
-   def edit
-   end
-   
-   def update
-   end
-   
-   def delete
-   end
+    def delete
+    end
 
 
-
+private
 def buyer_params
   params.require(:buyer).permit(:room, :bedroom)
 end
--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,11 +1,12 @@
 class Buyer < ActiveRecord::Base
+  
+  validates :room, presence: true, length: { maximum: 2 }
+  validates :bedroom, presence: true, length: { maximum: 2 }
 
   def self.search(search_room,search_bedroom)
     
-      where("room LIKE ? and bedroom LIKE ?", "%#{search_room}%", "%#{search_bedroom}%")
     
-    
+     where("room LIKE ? and bedroom LIKE ?", "%#{search_room}%", "%#{search_bedroom}%")
     
   end
-
 end
