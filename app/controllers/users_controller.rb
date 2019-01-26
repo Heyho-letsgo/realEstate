6a7186261b008a2a65d9f9d87002909fff7e0969
mori keita <keitamori@gmail.com>
2019-01-26 18:20:49 +0100

    Linting alignement du code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,31 @@
 class UsersController < ApplicationController
   def index
-  @users = User.all
-      
+    @users = User.all
   end
-    
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
-  end  
-    
+  end
+
   private
-  
+
   def params_user
     params.permit(:name,:email)
   end
-  
-  
-  
 end
