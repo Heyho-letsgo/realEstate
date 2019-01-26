45cc6ab596d728aced717816ccf991830223bcfb
mori keita <keitamori@gmail.com>
2019-01-26 12:13:38 +0100

    Alignement du code

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,85 +1,75 @@
 class SellersController < ApplicationController
   helper_method :sort_column, :sort_direction
 
-   def index
-       @sellers = Seller.all
-       @sellers = Seller.order("#{sort_column} #{sort_direction}").page params[:page]
-       
-   end
-   
-   def show
-      @seller = Seller.find(params[:id])
-   end
-   
-   def new
-      @seller = Seller.new
-   end
-   
-   def create
-      begin
-        @seller = Seller.new(seller_params)           
-        @seller.save!    #le point d'exclamation renvoie l'erreur au cas où - operation dangereuse avec un points d'exclamation
-        flash[:success] = "Seller created whith succes !"
-        redirect_to "/sellers"
-        rescue => quelque_probleme
-            puts quelque_probleme.message 
-            flash[:error] = "Seller creation failed! #{quelque_probleme.message}"
-            render 'new'
-        end
-   end
-   
-   def edit
-       @seller = Seller.find(params[:id])
-   end
-   
-   def update
-       @seller = Seller.find(params[:id])
-        if @seller.update_attributes(seller_params)
-            
-            flash[:success] = "Seller updated whith succes !"
-            redirect_to @seller
-            
-        else
-            flash[:error] = "Seller NOT updated whith succes !"
-            render 'edit'
-                
-       end
-    end
-   
-   def delete
-   end
+  def index
+    @sellers = Seller.all.order("#{sort_column} #{sort_direction}").page params[:page]
+  end
 
-    def search_sellers
-        render :search
-    end
+  def show
+    @seller = Seller.find(params[:id])
+  end
 
-   def search_results
+  def new
+    @seller = Seller.new
+  end
+
+  def create
+    begin
+      @seller = Seller.new(seller_params)           
+      @seller.save!    #le point d'exclamation renvoie l'erreur au cas où - operation dangereuse avec un points d'exclamation
+      flash[:success] = "Seller created whith succes !"
+      redirect_to "/sellers"
+    rescue => quelque_probleme
+      puts quelque_probleme.message 
+      flash[:error] = "Seller creation failed! #{quelque_probleme.message}"
+      render 'new'
+    end
+  end
+
+  def edit
+    @seller = Seller.find(params[:id])
+  end
+
+  def update
+    @seller = Seller.find(params[:id])
+    if @seller.update_attributes(seller_params)
+      flash[:success] = "Seller updated whith succes !"
+      redirect_to @seller
+    else
+      flash[:error] = "Seller NOT updated whith succes !"
+      render 'edit'
+    end
+  end
+
+  def delete
+  end
+
+  def search_sellers
+    render :search
+  end
+
+  def search_results
     search_room = params[:room]
     search_bedroom = params[:bedroom]
     @sellers = Seller.all
     @sellers = Seller.search(search_room, search_bedroom).order(created_at: :desc).page params[:page]
-        render :results_index
+    render :results_index
     @sellers = Seller.order("#{sort_column} #{sort_direction}")
-   end
-   
-    def from_buyers_to_sellers
-        @sellers = Seller.search(params[:buyer_id].to_i,'').page params[:page]
-        render :index
-    end
-        
-        
-    
-   
-   
-   private
+  end
+  def from_buyers_to_sellers
+    @sellers = Seller.search(params[:buyer_id].to_i,'').page params[:page]
+    render :index
+  end
 
-    def seller_params
-     params.permit(:room,:bedroom)
-    end
-    
-      def sortable_columns
+  private
+
+  def seller_params
+    params.permit(:room,:bedroom)
+  end
+
+  def sortable_columns
     ["id","room", "bedroom"]
-    end
+  end
 
   def sort_column
     sortable_columns.include?(params[:column]) ? params[:column] : "room"
