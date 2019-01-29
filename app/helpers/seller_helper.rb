d7239aa7c5d47a5f728f323615d5c6a88ed72ca5
mori keita <keitamori@gmail.com>
2019-01-29 19:53:39 +0100

    Prise de parametre de recherche

--- app/helpers/seller_helper.rb
+++ app/helpers/seller_helper.rb
@@ -1,9 +1,13 @@
 module SellerHelper
-  def sort_link(column, title = nil)
+  def sort_link(column, title = nil, data=nil)
     title ||= column.titleize
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
     icon = column == sort_column ? icon : ""
-    link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+    if data
+      link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction, room: data}
+    else
+      link_to "#{title} <span class='#{icon}'></span>".html_safe, {column: column, direction: direction}
+    end
   end
 end
